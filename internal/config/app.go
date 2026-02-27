acb78585a67a2268cb46799d4d2d3568c427c971
Kazgangap <halilkirazkaya@yandex.com>
2026-02-27 10:50:29 +0300

    update

--- internal/config/app.go
+++ internal/config/app.go
@@ -8,7 +8,7 @@ package config
 // Application Metadata
 // =============================================================================
 
-var Version = "1.0"
+var Version = "1.1"
 
 // Application info
 const (
