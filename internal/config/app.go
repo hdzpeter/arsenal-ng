bd811a3ac32fd794b6431a9240900a696beb0537
Kazgangap <90972683+halilkirazkaya@users.noreply.github.com>
2026-03-27 17:24:50 +0300

    Update app.go

--- internal/config/app.go
+++ internal/config/app.go
@@ -8,7 +8,7 @@ package config
 // Application Metadata
 // =============================================================================
 
-var Version = "1.2"
+var Version = "1.3"
 
 // Application info
 const (
